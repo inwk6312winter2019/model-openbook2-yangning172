04c2a79f6632e011abc41305f59cde268837d507
yangning172 <nn531884@dal.ca>
2019-02-06 13:29:32 -0800

    commit2

--- task1.py
+++ task1.py
@@ -1,9 +1,28 @@
-def extract_data('street_Centrelines.csv')
+def extract_data('street_Centrelines.csv')  #a tuple of (STR_NAME,FULL_NAME,FROM_STR,TO_STR)
     f = open('street_Centrelines.csv')
     list1 = []
     
     for line in f:
         list3 = line.split(',')
-        list1.append((list3[2],list3[4],list3[6],list3[7]))
+        list1.append((list3[2],list3[4],list3[6],list3[7]))    # theses index are to str_name, full_name,from_str,to_str)
     t = tuple(list1)
     return t
+
+
+
+
+def  histogram(file):
+    f = open('street_Centrelines.csv')
+    list2 = []
+    d = dict()
+    for line in f:
+        list4 = line.split(',')
+        list2.append(list4[12])    # the index[12] of  Maintenance
+    for n in list2:
+        if n not in d:
+            d[n] = 1
+        else:
+            d[n] += 1
+    return d
+
+
